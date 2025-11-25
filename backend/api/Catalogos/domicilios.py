a07abedbb26faa596f49fd0b63d349de98df7c3a
Edgar Esquivel <129891176+Emboesq13@users.noreply.github.com>
2025-11-25 09:29:09 -0600

    Actualizacion local 25/11/2025  9:29:09.42

--- backend/api/Catalogos/domicilios.py
+++ backend/api/Catalogos/domicilios.py
@@ -9,19 +9,20 @@ from backend.core.templates import templates
 router = APIRouter()
 
 
+PPeriodo: str = "2025-2026/1"
+HHost: str = "Test"
+
+
 @router.get("/domicilios", response_class=HTMLResponse)
 def domicilios_view(
     request: Request,
-    HHost: str = "Test",
-    PPeriodo: str = "2025-2026/1",
     db: Session = Depends(get_db)
 ):
     """
     Vista para consultar los domicilios mediante un Stored Procedure.
     """
-    UUsuario = request.cookies.get("nombre_usuario", "")
+    UUsuario = getUsuario(request)
     Rol = str(request.cookies.get("nombre_rol",""))
-
     try:
         # Ejecutar el Stored Procedure con parámetros nombrados
         query = text("""
@@ -85,45 +86,134 @@ def consultaEntidad(db: Session):
     
 
 @router.post("/registrarUA")
-def registrar_ua(data: dict, db: Session = Depends(get_db)):
+def registrar_ua(data: dict, request: Request, db: Session = Depends(get_db)):
     print("Datos recibidos en REGISTRAR:")
     print(data)  # <-- imprime todo el JSON recibido
+    try:
+        UUsuario = getUsuario(request)
+        query = text("""
+            EXEC [dbo].[SP_Alta_Catalogo_Unidad_Academica]
+                @UUnidad_Academica = :unidad_academica,
+                @NNombre = :nombre,
+                @CClave = :clave,
+                @DDirector = :director,
+                @IImagen = NULL,
+                @RRama = :rama,
+                @EEntidad = :entidad,
+                @MMunicipio = :municipio,
+                @CCalle = :calle,
+                @NNumero = :numero,
+                @CColonia = :colonia,
+                @CCP = :cp,
+                @UUsuario = :usuario,
+                @HHost = :host,
+                @PPeriodo = :periodo
+        """)
+        db.execute(query, {
+            "unidad_academica": data.get("sigla"),
+            "nombre": data.get("nombre"),
+            "clave": data.get("clave"),
+            "director": data.get("director"),
+            "rama": data.get("rama"),
+            "entidad": data.get("entidad"),
+            "municipio": data.get("municipio"),
+            "calle": data.get("calle"),
+            "numero":  int(data.get("numero")) if data.get("numero") else "SN",
+            "colonia": data.get("colonia"),
+            "cp": data.get("cp"),
+            "usuario": UUsuario,
+            "host": HHost, 
+            "periodo": PPeriodo
+        })
+        db.commit()
+        
+        print("UA registrada correctamente")
+    except Exception as e:
+        db.rollback()
+        print("Error al registrar UA:", e)
+        return {"status": "error", "msg": "Error al registrar UA"}
 
-    sigla = data.get("sigla")
-    entidad = data.get("entidad")
-    municipio = data.get("municipio")
-    localidad = data.get("localidad")
-
-    print("Sigla:", sigla)
-    print("Entidad:", entidad)
-    print("Municipio:", municipio)
-    print("Localidad:", localidad)
 
     return {"status": "ok", "msg": "UA registrada correctamente"}
 
 
-@router.put("/actualizarUA/{sigla}")
-def actualizar_ua(sigla: str, data: dict, db: Session = Depends(get_db)):
+@router.put("/actualizarUA/")
+def actualizar_ua(data: dict, request: Request, db: Session = Depends(get_db)):
 
     print("\nDatos recibidos en ACTUALIZAR:")
-    print("Sigla en ruta:", sigla)
     print("Body recibido:", data)
+    try:
+        UUsuario = getUsuario(request)
+        query = text("""
+            EXEC [dbo].[SP_Modifica_Catalogo_Unidad_Academica]
+                @UUnidad_Academica = :unidad_academica,
+                @NNombre = :nombre,
+                @CClave = :clave,
+                @DDirector = :director,
+                @IImagen = NULL,
+                @RRama = :rama,
+                @UUsuario = :usuario,
+                @HHost = :host,
+                @PPeriodo = :periodo
+        """)
+        db.execute(query, {
+            "unidad_academica": data.get("sigla"),
+            "nombre": data.get("nombre"),
+            "clave": data.get("clave"),
+            "director": data.get("director"),
+            "rama": data.get("rama"),
+            "usuario": UUsuario,
+            "host": HHost,
+            "periodo": PPeriodo
+        })
 
-    entidad = data.get("entidad")
-    municipio = data.get("municipio")
-    localidad = data.get("localidad")
 
-    print("Entidad:", entidad)
-    print("Municipio:", municipio)
-    print("Localidad:", localidad)
+        db.commit()
+        print("UA actualizada correctamente")
+    except Exception as e:
+        db.rollback()
+        print("Error al actualizar UA:", e)
+        return {"status": "error", "msg": "Error al actualizar UA"}
+    
 
-    return {"status": "ok", "msg": "UA actualizada correctamente"}
+    return {"status": "ok",  "msg": "UA actualizada correctamente"}
+
 
 
 
 @router.delete("/eliminarUA/{sigla}")
-def eliminar_ua(sigla: str, db: Session = Depends(get_db)):
+def eliminar_ua(sigla: str, request: Request, db: Session = Depends(get_db)):
     print("\nDatos recibidos en ELIMINAR:")
     print("Sigla:", sigla)
 
+    
+    try:
+        UUsuario = getUsuario(request)
+        query = text("""
+            EXEC [dbo].[SP_Baja_Catalogo_Unidad_Academica]
+                @UUnidad_Academica = :unidad_academica,
+                @UUsuario = :usuario,
+                @HHost = :host  ,
+                @PPeriodo = :periodo
+        """)    
+            
+        db.execute(query, {
+                "unidad_academica": sigla,
+                "usuario": UUsuario,
+                "host": HHost,
+                "periodo": PPeriodo
+            }
+        )
+        db.commit()
+        print("UA eliminada correctamente")
+    except Exception as e:
+        db.rollback()
+        print("Error al eliminar UA:", e)
+        return {"status": "error", "msg": str(e)}
+
     return {"status": "ok", "msg": "UA eliminada correctamente"}
+
+
+def getUsuario(request: Request):
+    UUsuario = request.cookies.get("nombre_usuario", "")
+    return UUsuario 
